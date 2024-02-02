2da2784dad6b251645bec5fc93cd019f7752d700
Jessica <jessica@plot.ly>
2024-02-02 17:20:46 -0500

    change layout to use dcc.Markdown

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from dash import Dash, html
+from dash import Dash, html, dcc
 
 app = Dash(__name__)
 
-app.layout = html.Div(children=[
-        html.H1(children='Hello World'),
-    ]
-),
-
+app.layout = dcc.Markdown('''
+    # Hello World
+''')
 
 if __name__ == '__main__':
     app.run(debug=True)
